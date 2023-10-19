52f4a946cfdb4923e1d9245621a80b67a72d7d52
Rodrigo Barro <rodrigo.correa.barro@gmail.com>
2023-10-18 21:58:17 -0300

    feat: add employee routes

--- app-js/src/config/index.js
+++ app-js/src/config/index.js
@@ -8,6 +8,9 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: false
+      }
     },
     pool: {
       min: 2,
--- app-js/src/index.js
+++ app-js/src/index.js
@@ -3,22 +3,67 @@ const knex = require(`${__dirname}/knex/knex.js`);
 
 api.get('/employee', async (req, res) => {
     try {
-        const pageNumber = 1; 
-        const pageSize = 10; 
+        const pageNumber = 1;
+        const pageSize = 100;
 
-        const employees = await knex('information_schema.tables')
-            .where('table_schema', 'public') 
+        const employees = await knex('employee')
             .limit(pageSize)
             .offset((pageNumber - 1) * pageSize)
-            .select('employee');
 
         return { employees };
     } catch (error) {
-        console.error('Error listing tables:', error);
-    } finally {
-        knex.destroy();
+        return { error: 'Unable to process request' }
     }
 });
+
+
+api.get('/employee/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const employees = await knex('employee')
+            .where({ id })
+
+        return { employees };
+    } catch (error) {
+        return { error: 'Unable to process request' }
+    }
+});
+
+api.delete('/employee/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        await knex('employee')
+            .where({ id })
+            .del();
+
+        return { message: 'employee successfully registered' };
+    } catch (error) {
+        return { error: 'Unable to process request' }
+    }
+});
+
+api.patch('/employee/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    try {
+        const result = await knex('employee').where({ id })
+            .update(data)
+        return result;
+    } catch (error) {
+        return { error: 'Unable to process request' }
+    }
+});
+
+api.post('/employee', async (req, res) => {
+    const data = req.body;
+    try {
+        await knex('employee').insert(data);
+        return { message: 'employee successfully registered' };
+    } catch (error) {
+        return { error: 'Unable to process request' }
+    }
+});
+
 
 exports.handler = async (event, context) => {
     return await api.run(event, context);
